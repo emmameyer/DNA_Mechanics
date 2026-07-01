2efcef9e1d43457818498196aedc92fe2e138de6
Emma Meyer <156262590+emmameyer@users.noreply.github.com>
2026-07-01 12:37:13 -0400

    Update merge_gro_files.py

--- code/merge_gro_files.py
+++ code/merge_gro_files.py
@@ -12,3 +12,16 @@ merged = Merge(ref.atoms, mob.atoms)
 merged.atoms.write("merged_system.gro")
 
 print("Merged system written to merged_system.gro")
+
+
+# i also have used the following code too:
+
+#import MDAnalysis as mda
+# Load the two gromacs files
+#u1 = mda.Universe("/Users/emma/Documents/kent/minicircles/BSC1/82bp_Tw8_deltaTw0/strand1_processed.gro")
+#u2 = mda.Universe("/Users/emma/Documents/kent/minicircles/BSC1/82bp_Tw8_deltaTw0/strand2_processed.gro")
+# Combine atoms from both universes
+#combined = mda.Merge(u1.atoms, u2.atoms)
+# Save to the same directory
+#output_path = "/Users/emma/Documents/kent/minicircles/BSC1/82bp_Tw8_deltaTw0/merged.gro"
+#combined.atoms.write(output_path)
